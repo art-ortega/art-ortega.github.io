631a23635f2c8ecc918e1c32eb292437d2942cff
Art-Ortega <artortega@Arts-MacBook-Pro.local>
2020-06-02 20:49:24 -0700

    added fonts

---
+++ build/precache-manifest.c24c63eaaf7445f5a69c756c75340e6f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5c8695189ff530b53a80e7b6161f662",
+    "url": "/portfoliop/index.html"
+  },
+  {
+    "revision": "a4ba958d18f6caceb90a",
+    "url": "/portfoliop/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a7cf9bfc4cbc3ae7501d",
+    "url": "/portfoliop/static/css/main.80e5d202.chunk.css"
+  },
+  {
+    "revision": "a4ba958d18f6caceb90a",
+    "url": "/portfoliop/static/js/2.8e833c84.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfoliop/static/js/2.8e833c84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7cf9bfc4cbc3ae7501d",
+    "url": "/portfoliop/static/js/main.cc068518.chunk.js"
+  },
+  {
+    "revision": "7b1bffa225b0787f1a93",
+    "url": "/portfoliop/static/js/runtime-main.82271195.js"
+  },
+  {
+    "revision": "26c4e63b6c5e499421e357b4242fedf5",
+    "url": "/portfoliop/static/media/battles.26c4e63b.gif"
+  },
+  {
+    "revision": "2db97d69ba44f8da4663584e8197143f",
+    "url": "/portfoliop/static/media/css.2db97d69.svg"
+  },
+  {
+    "revision": "1650bd9e0b086fa6e06a4e4e8427081c",
+    "url": "/portfoliop/static/media/dealnodeal.1650bd9e.gif"
+  },
+  {
+    "revision": "77eaba4cbb5e6d79c46cbaa8d6471dea",
+    "url": "/portfoliop/static/media/dinebud.77eaba4c.png"
+  },
+  {
+    "revision": "7445fd169656940835a3d3d1c52ca616",
+    "url": "/portfoliop/static/media/git.7445fd16.svg"
+  },
+  {
+    "revision": "8f8edc5fb64b9bd436834dc926ac992c",
+    "url": "/portfoliop/static/media/github.8f8edc5f.svg"
+  },
+  {
+    "revision": "42a4aedd09b52f141ba6819f79e9ea57",
+    "url": "/portfoliop/static/media/html5.42a4aedd.svg"
+  },
+  {
+    "revision": "dc2fc5be0984077b3b0fce8c0b8f045a",
+    "url": "/portfoliop/static/media/javascript.dc2fc5be.svg"
+  },
+  {
+    "revision": "e5289347c3de259559fcd065ca39dfb4",
+    "url": "/portfoliop/static/media/linked-circle.e5289347.svg"
+  },
+  {
+    "revision": "eeb234503462f3aa539bb9754955160b",
+    "url": "/portfoliop/static/media/me-circle.eeb23450.png"
+  },
+  {
+    "revision": "f16a2cda4a160e5aafbab5f2aa49c526",
+    "url": "/portfoliop/static/media/postgresql.f16a2cda.svg"
+  },
+  {
+    "revision": "159a804eb90e89fba5ec1c7678dede2e",
+    "url": "/portfoliop/static/media/prework-learn2.159a804e.png"
+  },
+  {
+    "revision": "2db2978200ab968daec54ec9630992b7",
+    "url": "/portfoliop/static/media/rails.2db29782.svg"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "/portfoliop/static/media/react.9a28da9f.svg"
+  },
+  {
+    "revision": "f571158fe4932e867040716341107c8b",
+    "url": "/portfoliop/static/media/resume-blue.f571158f.svg"
+  },
+  {
+    "revision": "4d25a557d152042f0b0f1ecaa0ea8d05",
+    "url": "/portfoliop/static/media/resume.4d25a557.pdf"
+  },
+  {
+    "revision": "514befa7a168a76156a2dd5a043d73fd",
+    "url": "/portfoliop/static/media/ruby.514befa7.svg"
+  }
+]);
